44836568bef1aad07a340129c2656d98b0bd77e8
sprater <scott.prater@wisc.edu>
2016-12-18 22:28:53 -0600

    Update copyright and version

--- src/main/java/org/dataaccessioner/DataAccessioner.java
+++ src/main/java/org/dataaccessioner/DataAccessioner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Seth Shaw.
+ * Copyright (C) 2016 Seth Shaw.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -39,7 +39,7 @@ import java.util.Set;
 public class DataAccessioner {
 
     private static final String NAME = "DataAccessioner";
-    private static final String VERSION = "1.0";
+    private static final String VERSION = "1.1";
     private static Logger logger;
 
     private Fits fits;
